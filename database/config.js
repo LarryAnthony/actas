0b28d0a7de4321a33cb24e5241b5ec61d12b990e
Larry <ajacobozare@gmail.com>
2021-05-10 21:05:16 -0500

    adding port 3001

--- database/config.js
+++ database/config.js
@@ -9,23 +9,23 @@ const { Pool, Client } = require('pg');
 //     port: 5432,
 //     ssl: { rejectUnauthorized: false }
 // });
-const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.DB,
-    database: 'pruebas',
-    password: process.env.PASSWORD,
-    port: 24362,
-    ssl: { rejectUnauthorized: false }
+const poolEnv = new Pool({
+	user: process.env.USER,
+	host: process.env.DB,
+	database: 'pruebas',
+	password: process.env.PASSWORD,
+	port: 24362,
+	ssl: { rejectUnauthorized: false }
 });
 
-const poolEnv = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'acta',
-    password: 12345678,
-    port: 5432
+const pool = new Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'acta',
+	password: 12345678,
+	port: 5432
 });
 module.exports = {
-    pool,
-    poolEnv
+	pool,
+	poolEnv
 }
